3d4f242ca59044502614d24fffa7e5987ba12220
Kevin Pena <kevinpena160@gmail.com>
2018-11-10 11:50:42 -0600

    working

--- client/src/components/Upload/Upload.js
+++ client/src/components/Upload/Upload.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import PDF from "react-pdf";
+import PDF from "../PDF";
 import "./Upload.css";
 // import { constants } from "fs";
 
